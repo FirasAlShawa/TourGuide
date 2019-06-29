9433535ec384972157fce8b07ca51a073aaaee9c
Firas shawa <firas.shawa22@gmail.com>
2019-06-29 22:33:03 +0300

    New Class places , Add Arraylist of placess in Gategory model

--- src/main/java/com/example/tour_guide/Category.java
+++ src/main/java/com/example/tour_guide/Category.java
@@ -1,21 +1,26 @@
 package com.example.tour_guide;
 
 
+import java.util.ArrayList;
+
 public class Category {
     private String type;
     private String icon;
     private int count;
+    private ArrayList<Place> places;
 
     public Category() {
         this.type = "Null";
         this.icon = "Null";
         this.count = 0;
+        this.places = new ArrayList<>();
     }
 
     public Category(String type, String icon, int count) {
         this.type = type;
         this.icon = icon;
         this.count = count;
+        this.places = new ArrayList<>();
     }
 
     public String getType() {
@@ -40,5 +45,13 @@ public class Category {
 
     public void setCount(int count) {
         this.count = count;
+    }
+
+    public ArrayList<Place> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(ArrayList<Place> places) {
+        this.places = places;
     }
 }
